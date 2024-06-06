3f88682d88433f42383fe63abe61322b9a067e2e
Andres Polo-Wood <andres.polo.wood@gmail.com>
2024-06-06 18:13:09 -0400

    main.js file has begun

--- src/server/main.js
+++ src/server/main.js
@@ -2,12 +2,14 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import dotenv from "dotenv";
-
+import mongoose from 'mongoose'
 
 // CONFIGURATION
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+mongoose.connect(process.env.MONGO_URI);
+console.log('connected to mongo: ', process.env.MONGO_URI)
 
 
 // Middleware to parse JSON requests
