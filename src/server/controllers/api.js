678b21075c7472c4b5e1e753af56da129f0e7535
Andres Polo-Wood <andres.polo.wood@gmail.com>
2024-06-09 11:53:23 -0400

    Can now fetch pokeCards with only specific data

--- src/server/controllers/api.js
+++ src/server/controllers/api.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import {createCard, getCard, getCards, updateCard, deleteCard} from'./pokeCardController.js';
+
 
 //Router
 const router = express.Router();
 
 //Routes
-router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.get('/cards', getCard);
-router.patch('/cards', updateCard);
-router.delete('/cards', deleteCard);
+
 
 router.get('*', (req, res) => {
     res.json('404')
