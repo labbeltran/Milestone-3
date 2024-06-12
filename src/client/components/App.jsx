e0f347acf073759b1db02401842882bd311fc695
labbeltran <labbeltran@gmail.com>
2024-06-11 20:22:01 -0400

    fixing main

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,42 +1,53 @@
-import { useEffect, useState } from 'react'
-import CardsGallery from './components/CardsGallery';
-// import Cards from './components/Cards';
-import SearchBar from './components/SearchBar'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import { selectUsers } from './store/usersSlice'
+// import {useSelector} from 'react-redux'
 
 function App() {
-  let [searchTerm, setSearchTerm] = useState('')
-  let [data, setData] = useState([])
-  let [message, setMessage] = useState('Whos that Pokemon!')
-
-  useEffect(() => {
-    if (searchTerm) {
-      document.title=`${searchTerm}, I choose you!`
-      const fetchData = async () => {
-        const response = await fetch(`https://api.pokemontcg.io/v2/cards?search?term=${searchTerm}`);
-        const resData = await response.json()
-        if(resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Not Found');
-          setData([]);
-        }
-      }
-      fetchData()
-  }
-  }, [searchTerm])
-
-  const handleSearch = (e, term) => {
-    e.preventDefault();
-    setSearchTerm(term);
-  };
+  const [count, setCount] = useState(0)
+//   const user = useSelector(selectUsers)
 
   return (
-    <div className="search-bar">
-      <SearchBar handleSearch={handleSearch} />
-      {message}
-      <CardsGallery data={data} />
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  //return (
+  //<>
+  //{
+  //user.currentUser ? 
+  //    <BrowserRouter>
+  //    <Routes>
+  //      <Route index element={<LoginPage />} />
+  //      <Route path="add-book" element={<AddBookPage />} />
+  //      <Route path="book/:id" element={<SingleBookPage />} />
+  //    </Routes>
+  //  </BrowserRouter>
+  //<LoginPage />
+  //} 
+  //this is how the dude has it for the firebase tutorial
+  )
 }
 
-export default App;
+export default App
+
