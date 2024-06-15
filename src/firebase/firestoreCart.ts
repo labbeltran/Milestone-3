35896a5e002bf6360dfefa4752e534088208474d
Andres Polo-Wood <andres.polo.wood@gmail.com>
2024-06-15 17:44:11 -0400

    SEARCH BAR WORKS

--- src/firebase/firestoreCart.ts
+++ src/firebase/firestoreCart.ts
@@ -23,21 +23,3 @@ export const updateFirestoreCart = async (cartItems: CartItem[]): Promise<void> 
         await batch.commit();
     }
 };
-
-export async function fetchItemPrice(itemId: string): Promise<number> {
-    try {
-      const itemRef = doc(firestore, 'cart', itemId); // Adjust 'storeItems' to your Firestore collection
-      const itemDoc = await getDoc(itemRef);
-  
-      if (itemDoc.exists()) {
-        const itemData = itemDoc.data();
-        const price = itemData.price; // Assuming 'price' is a field in your Firestore document
-        return price;
-      } else {
-        throw new Error(`Document ${itemId} not found in cart`);
-      }
-    } catch (error) {
-      console.error('Error fetching item price:', error);
-      throw error; // Rethrow the error to be handled where fetchItemPrice is called
-    }
-  }
