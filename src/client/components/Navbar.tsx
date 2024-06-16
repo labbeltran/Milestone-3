3e92ef8af641a7f899ea9db1e7a8a23fc98b079e
labbeltran <labbeltran@gmail.com>
2024-06-15 23:08:53 -0400

    getting ready to deploy that backend

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -53,7 +53,7 @@ export function NavBar() {
               className="rounded-circle"
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
+                xmlns="www.w3.org/2000/svg"
                 viewBox="0 0 576 512"
                 fill="currentColor"
               >
--- src/server/main.js
+++ src/server/main.js
@@ -4,6 +4,7 @@ import ViteExpress from "vite-express";
 import dotenv from "dotenv";
 import apisRouter from './routes/api.js'
 
+
 // CONFIGURATION
 dotenv.config();
 const app = express();
@@ -11,9 +12,19 @@ const PORT = process.env.PORT;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(express.static('dist'));
+app.use(express.urlencoded({ extended: true}));
+
+//Serve Static Files
+const __dirname = 'dist/index.html'
+
 
 // ROUTES
 app.use('/api', apisRouter)
+app.get('*', (req, res)=>{
+  res.sendFile('dist/index.html', {root:__dirname})
+})
+
 
 //Listen
 ViteExpress.listen(app, PORT, () =>
